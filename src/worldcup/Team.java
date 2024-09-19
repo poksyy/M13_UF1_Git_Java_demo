04d53cf4655659a954687bcc10f9a89919af4ff8
Pau Carrera <poksy2002@gmail.com>
2024-09-19 16:56:36 +0200

    testing commit

--- src/worldcup/Team.java
+++ src/worldcup/Team.java
@@ -16,7 +16,7 @@ public class Team {
     
     
     public void play(){
-            System.out.println("playing");
+            System.out.println("players are playing");
         }
 
     public void attack(){
